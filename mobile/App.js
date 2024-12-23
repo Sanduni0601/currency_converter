30695deb34f35417277ec067f6d6cd31cba40caa
Sanduni Nisansala <139616844+Sanduni0601@users.noreply.github.com>
2024-12-23 09:53:07 +0000

    Set input field to enter currency value

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput} from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
       
-      <StatusBar style="auto" />
+     <TextInput style={styles.input}
+     mode="outlined"
+     placeholder = "Enter amount in LKR"
+     />
     </View>
   );
 }
@@ -18,4 +21,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+  }
 });
